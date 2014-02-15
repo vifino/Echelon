33f56cf3f6fa30f6e28b3b41939796a40527de99
vifino <adinator1999@gmail.com>
2014-02-15 23:28:51 +0100

    Added Example Config

---
+++ config.js
@@ -0,0 +1,14 @@
+// Put your config here!
+var channel = ["#ccbots"];
+var server = "irc.esper.net";
+var port = 5555;
+var nick = "Echelon";
+var pass = "";
+var realName = "vifino's Scripted Bot";
+var botMaster = "vifino";
+exports.channel = channel;
+exports.server = server;
+exports.port = port;
+exports.nick = nick;
+exports.realName = realName;
+exports.botMaster = botMaster;
