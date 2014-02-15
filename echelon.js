e554320dc59f984584f148dda8b6c90fe9b259f1
vifino <adinator1999@gmail.com>
2014-02-16 00:21:10 +0100

    Update echelon.js

--- echelon.js
+++ echelon.js
@@ -14,10 +14,6 @@ var bot = new irc.Client(config.server, config.nick, {
 if (!config.pass == "") {
 	bot.say("nickserv", "identify " + config.pass);
 };
-var date = new Date();
-var current_hour = date.getHours();
-var current_min = date.getMinutes();
-var current_sec = date.getSeconds();
 var modulestarted = [];
 // Some First-Run Configuration
 if (!fs.existsSync("./modules")) {
@@ -61,6 +57,10 @@ bot.addListener("message", function(from, to, text, message) {
 	if (text.toLowerCase() == config.nick.toLowerCase() + " time")
 	  {
 		console.log("User " + from + " Requested Time.");
+		var date = new Date();
+	var current_hour = date.getHours();
+	var current_min = date.getMinutes();
+	var current_sec = date.getSeconds();
 		bot.say(config.channel[0],"Current Time: " + current_hour + ":" + current_min + ":" + current_sec)
 	  }
 	else if (text.toLowerCase() == config.nick.toLowerCase() + " logout")
