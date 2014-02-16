2f3fa3107cadfc075fe4a93de0f4a80307c8599b
vifino <adinator1999@gmail.com>
2014-02-16 15:27:48 +0100

    Update testmod.js

--- modules/testmod.js
+++ modules/testmod.js
@@ -1,7 +1,9 @@
 function start() {
+// Do your setup here:
 console.log("testmod started")
 }
 function execute(from,to,bot,config,echexecargs) {
+// When the module is called with "(Nick) execute: (Module)", this gets executed.
 bot.say(config.channel[0], "Hi I'm the Test Module! My Owner is " + config.botMaster + "." );
 }
 exports.start = start;
