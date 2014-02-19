1cd1cebfe3e67ad7be8ab3a728343abde4b40deb
MrRatermat <ratermat@gmail.com>
2014-02-19 20:39:48 +0000

    Update cookies.js

--- modules/cookies.js
+++ modules/cookies.js
@@ -1,16 +1,16 @@
 //Cookie counter
 
-var cookies = 0;
+var cookies=0;
 
 function start(from,to,bot,config,echexecargs) {
-  console.log("I hope you have cookies enabled")
+  console.log("I hope you have cookies enabled");
   bot.addListener("message", function(from, to, text, message) {
     bot.say(config.channel[0], "I hope you have cookies enabled");
     if (text.toLowerCase() == config.nick.toLowerCase()+ " +cookie")
     {
-      cookies += 1;
+      cookies+=1;
     } elseif (text.toLowerCase() == config.nick.toLowerCase()+ " -cookie")
-      cookies -= 1;
+      cookies-=1;
     {
     
     } elseif (text.toLowerCase() == config.nick.toLowerCase()+ " cookies")
