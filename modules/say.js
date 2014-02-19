cef93bd1778c4c15f745a560990aae3eca9dbecc
vifino <adinator1999@gmail.com>
2014-02-19 23:33:34 +0100

    Update say.js

--- modules/say.js
+++ modules/say.js
@@ -4,11 +4,10 @@
 function start(from,to,bot,config,echexecargs) {
   console.log("Adding listener for say");
   bot.addListener("message", function(from, to, text, message) {
-    if(text.toLowerCase().substring(0,config.nick.lenght() + 5) == config.nick.toLowerCase() + " say ")
+    var nick = config.nick;
+    if(text.toLowerCase().substring(0, nick.lenght + 5) == config.nick.toLowerCase() + " say ")
     {
-      
-      
-      bot.say(config.channel[0], text.toLowerCase().substring(config.nick.lenght() + 6));
+      bot.say(config.channel[0], text.substring(nick.lenght + 6));
     };
   });
 }
