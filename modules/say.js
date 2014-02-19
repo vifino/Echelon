cd20e5463b0af71a77256b42be9fedc3957e3f74
MrRatermat <ratermat@gmail.com>
2014-02-19 22:38:21 +0000

    Case insensitive

--- modules/say.js
+++ modules/say.js
@@ -4,13 +4,10 @@
 function start(from,to,bot,config,echexecargs) {
   console.log("Adding listener for say");
   bot.addListener("message", function(from, to, text, message) {
-    var nick = config.nick;
     if(text.toLowerCase().indexOf(config.nick.toLowerCase()+" say ") == 0)
-      {
-        var regEx = new RegExp(config.nick+" say ", "ig");
-        var result = text.replace(regEx, "");
-       
-        bot.say(config.channel[0], text.replace(regEx, ""));
+    {
+      var regEx = new RegExp(config.nick+" say ", "ig");
+      bot.say(config.channel[0], text.replace(regEx, ""));
     };
   });
 }
