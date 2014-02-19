8398a152817a81a58da228a4607ee64192963b03
vifino <adinator1999@gmail.com>
2014-02-19 21:40:52 +0100

    Update cookies.js

--- modules/cookies.js
+++ modules/cookies.js
@@ -13,15 +13,18 @@ function start(from,to,bot,config,echexecargs) {
       cookies -= 1;
     {
     
-    } elseif (text.toLowerCase() == config.nick.toLowerCase()+ " cookies")
+    } 
+    else if (text.toLowerCase() == config.nick.toLowerCase()+ " cookies")
     {
       if (cookies>0)
       {
         bot.say(config.channel[0], "I have "+cookies+" cookies!");
-      } elseif (cookies<0)
+      } 
+      else if (cookies<0)
       {
         bot.say(config.channel[0], "I'm in debt, I owe "+(-cookies)+" cookies...");
-      } else {
+      } 
+      else {
         bot.say(config.channel[0], "I don't have any cookies.");
       }
     }
