550f6be4ffdd4d6b32ea14b57caf2a50a1310f67
MrRatermat <ratermat@gmail.com>
2014-02-19 20:33:59 +0000

    Cookies.js
    Counts how many items Echelon has

---
+++ modules/cookies.js
@@ -0,0 +1,33 @@
+//Cookie counter
+
+int cookies
+
+function start(from,to,bot,config,echexecargs) {
+  console.log("Adding listener for cookies.js")
+  bot.addListener("message", function(from, to, text, message) {
+    if (text.toLowerCase() == config.nick.toLowerCase()+ " +cookie")
+    {
+      cookies+=1;
+    } elseif (text.toLowerCase() == config.nick.toLowerCase()+ " -cookie")
+      cookies-=1;
+    {
+    
+    } elseif (text.toLowerCase() == config.nick.toLowerCase()+ " cookies")
+    {
+      if (cookies>0)
+      {
+        bot.say(config.channel[0], "I have "+cookies+" cookies!");
+      } elseif (cookies<0)
+      {
+        bot.say(config.channel[0], "I'm in debt, I owe "+(-cookies)+" cookies...");
+      } else {
+        bot.say(config.channel[0], "I don't have any cookies.");
+      }
+    }
+  });
+}
+function execute() {
+  // Nothing
+}
+exports.start = start;
+exports.execute = execute;
