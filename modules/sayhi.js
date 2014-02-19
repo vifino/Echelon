2b65f5da63e2ed0870eaa04852787737a6d182b8
vifino <adinator1999@gmail.com>
2014-02-19 20:59:10 +0100

    Update sayhi.js

--- modules/sayhi.js
+++ modules/sayhi.js
@@ -1,9 +1,12 @@
-function start() {
+function start(from,to,bot,config,echexecargs) {
   bot.addListener("message", function(from, to, text, message) {
     if (text.toLowerCase() == "hello " + config.nick.toLowerCase())
     {
       bot.say(config.channel[0], "Hello, "+from+ "." );
     };
   });
+}
+function execute() {
+  // Nothing
 }
 exports.start = start;
