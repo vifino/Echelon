d603322c7be0ef55caea72ea97fcfc2ae3cd647f
MrRatermat <ratermat@gmail.com>
2014-02-19 21:47:13 +0000

    Update say.js

--- modules/say.js
+++ modules/say.js
@@ -2,11 +2,11 @@
 //Usage: [nick] say [msg]
 
 function start(from,to,bot,config,echexecargs) {
-  console.log("Adding listener for say")
+  console.log("Adding listener for say");
   bot.addListener("message", function(from, to, text, message) {
     if(text.toLowerCase().indexOf(config.nick.toLowerCase()+" say ") == 0)
     {
-      bot.say(config.channel[0], text.replace(config.nick.toLowerCase()+" say ",""));
+      bot.say(config.channel[0], text.toLowerCase().replace(config.nick.toLowerCase()+" say ",""));
     };
   });
 }
