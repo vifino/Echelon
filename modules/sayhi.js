b7d862ffab3add1a682af0ed6a5b0ec5a55cee63
MrRatermat <ratermat@gmail.com>
2014-02-19 19:47:47 +0000

    Console vanity

--- modules/sayhi.js
+++ modules/sayhi.js
@@ -1,7 +1,9 @@
 function start() {
+  console.log("Adding 'Hello' Listeners")
   bot.addListener("message", function(from, to, text, message) {
     if (text.toLowerCase() == "hello " + config.nick.toLowerCase())
     {
+      console.log("Saying hello to user")
       bot.say(config.channel[0], "Hello, "+from+ "." );
     };
   });
