b6cab2b90cbdf8e3461d304a4d3e59c335d75e41
MrRatermat <ratermat@gmail.com>
2014-02-20 14:37:35 +0000

    Update testmod.js
    Update to testmod.js so it follows the standards the current module code runs with.

--- modules/testmod.js
+++ modules/testmod.js
@@ -1,10 +1,10 @@
-function start() {
-// Do your setup here:
-console.log("testmod started")
+function start(from,to,bot,config,echexecargs) {
+// Do your setup here
 }
-function execute(from,to,bot,config,echexecargs) {
-// When the module is called with "(Nick) execute: (Module)", this gets executed.
-bot.say(config.channel[0], "Hi I'm the Test Module! My Owner is " + config.botMaster + "." );
+function execute() {
+  // When the module is called with "(Nick) execute: (Module)", this gets executed.
+  console.log("testmod called");
+  bot.say(config.channel[0], "Hi I'm the Test Module! My Owner is " + config.botMaster + "." );
 }
 exports.start = start;
 exports.execute = execute;
