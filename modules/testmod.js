93b204b431f6827227130023b144975e644e5d0b
MrRatermat <ratermat@gmail.com>
2014-02-20 14:39:44 +0000

    Slight change, again

--- modules/testmod.js
+++ modules/testmod.js
@@ -1,7 +1,7 @@
-function start(from,to,bot,config,echexecargs) {
+function start() {
 // Do your setup here
 }
-function execute() {
+function execute(from,to,bot,config,echexecargs) {
   // When the module is called with "(Nick) execute: (Module)", this gets executed.
   console.log("testmod called");
   bot.say(config.channel[0], "Hi I'm the Test Module! My Owner is " + config.botMaster + "." );
