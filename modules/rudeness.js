a71c977681c30f23e902e6c8111f300999703d53
vifino <adinator1999@gmail.com>
2014-02-21 16:25:54 +0100

    Create rudeness.js

---
+++ modules/rudeness.js
@@ -0,0 +1,58 @@
+//rudeness module
+//usage: rude word [nick]
+//rude word here.
+
+var replies =
+[
+	"Go fall down a hole",
+	"You STFU",
+	"You're a retard"
+];
+
+function rudeMod(bot, confic)
+{
+	console.log("Adding listener for rude.js");
+	bod.addListener("message", function(from, to, text, message)
+	{
+		var fwordIndex = text.toLowerCase().indexOf("Fuck you");
+		var damnIndex = text.toLowerCase().indexOf("Damn you");
+		var stfuIndex = text.toLowerCase().indexOf("STFU");
+		var nickIndex = text.toLowerCase().indexOf(config.nick.toLowerCase());
+		
+		var index = -1;
+		
+		if (fwordIndex != -1)
+			index = fwordIndex;
+		if (damnIndex != -1)
+			index = damnIndex;
+		if (stfuIndex != -1)
+			index = stfuIndex;
+			
+		if (index != -1 && nickIndex != -1 && index < nickIndex)
+		{
+			var randNumber = Math.floor(Math.random()*replies.length);
+			bot.say(config.channel[0], replies[randNumber] + ", " + from + ".");
+		}
+	});
+}
+
+function start(from, to, bot, config, echexecargs)
+{
+	console.log("Rude started manually");
+	rudeInit(bot, config);
+}
+
+function autorun(bot, config)
+{
+	console.log("Rude started automatically");
+	rudeInit(bot, config);
+}
+
+function execute
+{
+	//nothing
+}
+
+exports.start = start;
+exports.execute = execute;
+exports.autoload = autorun;
