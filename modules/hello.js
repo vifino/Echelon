788fa3f9b6bacd73736295b0235092def014a96e
MrRatermat <ratermat@gmail.com>
2014-02-21 14:09:57 +0000

    Added "Hey" and "Hi"

--- modules/hello.js
+++ modules/hello.js
@@ -12,8 +12,16 @@ function helloInit(bot, config) {
   console.log("Adding listener for hello.js");
   bot.addListener("message", function(from, to, text, message) {
     var HelloIndex=text.toLowerCase().indexOf("hello");
+    var HiIndex=text.toLowerCase().indexOf("hi");
+    var HeyIndex=text.toLowerCase().indexOf("hey");
     var NickIndex=text.toLowerCase().indexOf(config.nick.toLowerCase());
-    if (HelloIndex!=-1 && NickIndex!=-1 && HelloIndex<NickIndex)
+    
+    var index=-1;
+    if (HelloIndex!=-1) index=HelloIndex;
+    if (HiIndex!=-1) index=HiIndex;
+    if (HeyIndex!=-1) index=HeyIndex;
+    
+    if (index!=-1 && NickIndex!=-1 && index<NickIndex)
     {
       var randomNumber = Math.floor(Math.random()*Replylist.length);
       bot.say(config.channel[0], Replylist[randomNumber]+", "+from+ "." );
