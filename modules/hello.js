86448f6a933ee64168f2324efa412995e8487262
MrRatermat <ratermat@gmail.com>
2014-02-26 18:34:59 +0000

    Update hello.js

--- modules/hello.js
+++ modules/hello.js
@@ -11,47 +11,41 @@ var Replylist = [
 var listenfor = ["hello","hi","hey","sup"]
 var msgto;
 function helloInit(bot, config) {
-  console.log("Adding listener for hello.js");
-  bot.addListener("message", function(from, to, text, message) {
-	if (!(to == config.nick)) {
-		//Not PM
-		msgto=to;
-	}
-	else {
-		msgto=from;
-	};
-    var NickIndex=text.toLowerCase().indexOf(config.nick.toLowerCase());
-    var index=-1;
-    for (var i=0;i<listenfor.length;i++)
-    {
-      var iof=text.toLowerCase().indexOf(listenfor[i].toLowerCase());
-      if (iof!=-1)
-      {
-        index=iof;
-        break;
-      }
-    }
-    
-    if (index!=-1 && NickIndex!=-1 && index<NickIndex)
-    {
-      var randomNumber = Math.floor(Math.random()*Replylist.length);
-      bot.say(msgto, Replylist[randomNumber]+", "+from+ "." );
-    };
-  });
+	console.log("Adding listener for hello.js");
+	bot.addListener("message", function(from, to, text, message) {
+		if (to != config.nick) msgto=to; else msgto=from;
+		var NickIndex=text.toLowerCase().indexOf(config.nick.toLowerCase());
+		var index=-1;
+		for (var i=0;i<listenfor.length;i++)
+		{
+			var iof=text.toLowerCase().indexOf(listenfor[i].toLowerCase());
+			if (iof!=-1)
+			{
+				index=iof;
+				break;
+			}
+		}
+		
+		if (index!=-1 && NickIndex!=-1 && index<NickIndex)
+		{
+			var randomNumber = Math.floor(Math.random()*Replylist.length);
+			bot.say(msgto, Replylist[randomNumber]+", "+from+ "." );
+		};
+	});
 }
 
 function start(from,to,msgto,bot,config,echexecargs) {
-  console.log("Hello started manually");
-  helloInit(bot, config);
+	console.log("Hello started manually");
+	helloInit(bot, config);
 }
 
 function autorun(bot, config) {
-  console.log("Hello started automatically");
-  helloInit(bot, config);
+	console.log("Hello started automatically");
+	helloInit(bot, config);
 }
 
 function execute() {
-  // Nothing
+	// Nothing
 }
 exports.start = start;
 exports.execute = execute;
