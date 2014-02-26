67ed05f07b3b5cae1b0c38e00999b849f2213414
MrRatermat <ratermat@gmail.com>
2014-02-26 18:36:48 +0000

    Update say.js

--- modules/say.js
+++ modules/say.js
@@ -1,36 +1,31 @@
 //Say function
 //Usage: [nick] say [msg]
-var msgto;
+
 function initSay(bot, config)
 {
-    console.log("Adding listener for say");
-  bot.addListener("message", function(from, to, text, message) {
-	if (!(to == config.nick)) {
-		//Not PM
-		msgto=to;
-	}
-	else {
-		msgto=from;
-	};
-    if(text.toLowerCase().indexOf(config.nick.toLowerCase()+" say ") == 0)
-    {
-      bot.say(msgto, text.replace(",","").replace(":","").substring((config.nick+" say ").length,text.length));
-    };
-  });
+	console.log("Adding listener for say");
+	bot.addListener("message", function(from, to, text, message) {
+		var msgto;
+		if (to != config.nick) msgto=to; else msgto=from;
+		if(text.toLowerCase().indexOf(config.nick.toLowerCase()+" say ") == 0)
+		{
+			bot.say(msgto, text.replace(",","").replace(":","").substring((config.nick+" say ").length,text.length));
+		};
+	});
 }
 
 function start(from,to,msgto,bot,config,echexecargs) {
-  console.log("say.js started manually")
-  initSay(bot, config);
+	console.log("say.js started manually")
+	initSay(bot, config);
 }
 
 function autorun(bot,config) {
-  console.log("say.js started automatically")
-  initSay(bot, config);
+	console.log("say.js started automatically")
+	initSay(bot, config);
 }
 
 function execute() {
-  // Nothing
+	// Nothing
 }
 exports.start = start;
 exports.execute = execute;
