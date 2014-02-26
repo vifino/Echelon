690a24c45482ee7dcf37ba8d3ee180f57378cdc2
MrRatermat <ratermat@gmail.com>
2014-02-26 20:04:46 +0000

    Cleaned up code slightly

--- modules/10.js
+++ modules/10.js
@@ -1,88 +1,82 @@
 var questions;
-var GameStarted=false;
-var Player="";
-var Min=0;
-var Max=100;
-var lastquestion=0;
-var lastvalue=0;
+var GameStarted = false;
+var Player = "";
+var Min = 0;
+var Max = 100;
+var lastquestion = 0;
+var lastvalue = 0;
 var Timeout;
 
-function start(from, to,msgto , bot, config, echexecargs) {
+function start(from, to, msgto, bot, config, echexecargs) {
     // Do your setup here
-    bot.addListener("message", function(from, to, text, message) {
+    bot.addListener("message", function (from, to, text, message) {
         var msgto;
-        if (to != config.nick) msgto=to; else msgto=from;
-        if (GameStarted && from==Player)
-        {
-            var Yes=(text.toLowerCase().indexOf("yes")>-1);
-            var No=(text.toLowerCase().indexOf("no")>-1);
+        if (to != config.nick) msgto = to;
+        else msgto = from;
+        if (GameStarted && from == Player) {
+            var Yes = (text.toLowerCase().indexOf("yes") > -1);
+            var No = (text.toLowerCase().indexOf("no") > -1);
             if (Yes != No) //XOR
             {
-                if (lastquestion==1) {
-                    if (Yes) Min=lastvalue;
-                    if (No) Max=lastvalue;
-                } else if (lastquestion==2) {
-                    if (Yes)
-                    {
-                        bot.say(msgto , "Yay, I win.");
-                        GameStarted=false;
-                        Player="";
+                if (lastquestion == 1) {
+                    if (Yes) Min = lastvalue;
+                    if (No) Max = lastvalue;
+                } else if (lastquestion == 2) {
+                    if (Yes) {
+                        bot.say(msgto, "Yay, I win.");
+                        GameStarted = false;
+                        Player = "";
                     };
                 };
-                if (Min==Max)
-                {
+                if (Min == Max) {
                     bot.say(msgto, "You're cheating");
-                    GameStarted=false;
-                    Player="";
+                    GameStarted = false;
+                    Player = "";
                 }
-                if (GameStarted)
-                {
+                if (GameStarted) {
                     AskQuestion(bot, msgto);
                 }
             };
-            if (text.toLowerCase().indexOf("debug")>-1)
-            { //Debug
-                bot.say(msgto, "Your number is between "+Min+" and "+Max+".");
+            if (text.toLowerCase().indexOf("debug") > -1) { //Debug
+                bot.say(msgto, "Your number is between " + Min + " and " + Max + ".");
             };
         };
     });
 };
 
-function AskQuestion(bot, msgto)
-{
-    if (questions<=1) {
-        bot.say(msgto,"Damn it, I lose!");
-        GameStarted=false;
+function AskQuestion(bot, msgto) {
+    if (questions <= 1) {
+        bot.say(msgto, "Damn it, I lose!");
+        GameStarted = false;
     } else {
-        questions-=1;
-        
-        if (Math.abs(Max-Min)<5 || questions<2) { //Take a guess
-            lastquestion=2;
-            lastvalue = Math.floor((Math.random()*(Max-Min))+Min);
-            bot.say(msgto,"Question "+questions+": Is your number "+lastvalue+"?");
+        questions -= 1;
+
+        if (Math.abs(Max - Min) < 5 || questions < 2) { //Take a guess
+            lastquestion = 2;
+            lastvalue = Math.floor((Math.random() * (Max - Min)) + Min);
+            bot.say(msgto, "Question " + questions + ": Is your number " + lastvalue + "?");
         } else {
-            lastquestion=1;
-            lastvalue = Math.floor((Math.random()*(Max-Min))+Min);
-            bot.say(msgto,"Question "+questions+": Is your number over "+lastvalue+"?");
+            lastquestion = 1;
+            lastvalue = Math.floor((Math.random() * (Max - Min)) + Min);
+            bot.say(msgto, "Question " + questions + ": Is your number over " + lastvalue + "?");
         }
     }
 }
 
-function execute(from,to,msgto,bot,config,echexecargs) {
-    console.log("10.js started by "+from);
+function execute(from, to, msgto, bot, config, echexecargs) {
+    console.log("10.js started by " + from);
     var date = new Date();
-    if (!GameStarted || from == Player || date>Timeoue)
-    {
-        questions=10;
-        Min=0;
-        Max=100;
-        bot.say(msgto, from +", think of a number between 0 and 100. I will then try to guess it in "+questions+" questions.");
-        GameStarted=true;
-        var Timeout = new Date(date.getTime() + (60*60000));
-        Player=from;
+    if (!GameStarted || from == Player || date > Timeoue) {
+        questions = 10;
+        Min = 0;
+        Max = 100;
+        bot.say(msgto, from + ", think of a number between 0 and 100. I will then try to guess it in " + questions + " questions.");
+        GameStarted = true;
+        var Timeout = new Date(date.getTime() + (60 * 60000));
+        Player = from;
         AskQuestion(bot, msgto);
     } else {
-        bot.say(msgto,"I am currently in a game with "+Player+". Game expires in "+current_min + ":" + current_sec)
+        bot.say(msgto, "I am currently in a game with " + Player + ". Game expires in " + current_min + ":" + current_sec)
     }
 };
 
