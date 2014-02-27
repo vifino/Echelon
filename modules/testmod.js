05acf79bbf978d8be7cae3b4257dafa183c7a660
MrRatermat <ratermat@gmail.com>
2014-02-27 21:35:34 +0000

    Trimmed code, added info

--- modules/testmod.js
+++ modules/testmod.js
@@ -1,16 +1,14 @@
 // A basic TestModule
-var msgto;
-function start() {
-// Do your setup here
-}
+// Usage: Manual execution
+// Displays debug info
+
 function execute(from,to,msgto,bot,config,echexecargs) {
-  // When the module is called with "(Nick) execute: (Module)", this gets executed.
+	// When the module is called with "(Nick) execute: (Module)", this gets executed.
 	console.log("testmod called");
 	console.log("from: "+ from);
 	console.log("to: " + to);
 	console.log("MSG: "+ msgto);
-	bot.say(msgto, "Hi I'm the Test Module! Extra info here 2" );
+	bot.say(msgto, "Hi I'm the Test Module!" );
 	bot.say(msgto, "Master[" + config.botMaster + "], from["+from+"], "+"to["+to+"], "+"msgto["+msgto+"]" );
 }
-exports.start = start;
 exports.execute = execute;
