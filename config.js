5a10de64ee4e86223874c96a37f90b8725fe7c54
andrejsavikin <andrejsavikin@users.noreply.github.com>
2014-06-10 18:09:47 +0200

    Cleaned up code
    Yay for efficiency :D

--- config.js
+++ config.js
@@ -1,15 +1,8 @@
 // Put your config here!
-var channel = ["#ccbots"];
-var server = "irc.esper.net";
-var port = 5555;
-var nick = "Echelon";
-var pass = "";
-var realName = "vifino's Scripted Bot";
-var botMaster = "vifino";
-exports.channel = channel;
-exports.server = server;
-exports.port = port;
-exports.nick = nick;
-exports.pass = pass;
-exports.realName = realName;
-exports.botMaster = botMaster;
+exports.channel = ["#ccbots"];
+exports.server = "irc.esper.net";
+exports.port = 5555;
+exports.nick = "Echelon";
+exports.pass = "";
+exports.realName = "vifino's Scripted Bot";
+exports.botMaster = "vifino";
